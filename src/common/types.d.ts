8443a18927edaed150e50f09368b6612df7d55bb
nabepa <nabepadev@gmail.com>
2021-05-28 11:29:59 +0900

    clean code & some meta info

--- src/common/types.d.ts
+++ src/common/types.d.ts
@@ -26,6 +26,3 @@ type TasksData = {
 type AddTask = (columnId: ColumnId, task: Task) => void;
 
 type RemoveTask = (columnId: ColumnId, task: Task, index: number) => void;
-// type ToggleTodo = (selectedTodo: Todo) => void;
-
-// type AddTodo = (text: string) => void;
